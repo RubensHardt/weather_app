74f60339fc791f602af8616d938759186fe06174
Rubens Hardt Junior <rubenshardtster@gmail.com>
2022-08-02 16:44:52 -0300

    Add weather repository

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,14 @@
+import '../data_sources/remote_data_source.dart';
+import '../models/weather.dart';
+
+class WeatherRepository {
+  final RemoteDataSource remoteDataSource;
+
+  WeatherRepository({
+    required this.remoteDataSource,
+  });
+
+  Future<List<Weather>?> getCurrentWeathers() async {
+    return await remoteDataSource.getCurrentWeathers();
+  }
+}
