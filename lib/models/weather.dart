ea3cc0a9117a7c5cc0875becc9c9fcae90e2002b
Rubens Hardt Junior <rubenshardtster@gmail.com>
2022-08-02 16:25:38 -0300

    Add weather model

---
+++ lib/models/weather.dart
@@ -0,0 +1,25 @@
+class Weather {
+  final int cityCode;
+
+  final String city;
+
+  final String countryCode;
+
+  final double temperature;
+
+  final String state;
+
+  final String iconCode;
+
+  final String description;
+
+  Weather({
+    required this.cityCode,
+    required this.city,
+    required this.countryCode,
+    required this.temperature,
+    required this.state,
+    required this.iconCode,
+    required this.description,
+  });
+}
