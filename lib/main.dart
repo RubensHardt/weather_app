40a594f565e986666c8f708cfa51801e13aee6ee
Rubens Hardt Junior <rubenshardtster@gmail.com>
2022-08-02 17:08:30 -0300

    Setup weather repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_app/repositories/weather_repository.dart';
 
+import 'data_sources/local_data_source/local_data_source.dart';
 import 'data_sources/local_data_source/models/weathers_list.dart';
+import 'data_sources/remote_data_source/remote_data_source.dart';
 import 'models/weather.dart';
+
+final getIt = GetIt.instance;
 
 void main() async {
   await setupHive();
@@ -14,6 +21,13 @@ Future<void> setupHive() async {
   Hive
     ..registerAdapter(WeatherAdapter())
     ..registerAdapter(WeathersListAdapter());
+}
+
+void setupWeatherRepository() {
+  getIt.registerSingleton(WeatherRepository(
+    remoteDataSource: RemoteDataSource(Dio()),
+    localDataSource: LocalDataSource(Hive),
+  ));
 }
 
 class MyApp extends StatelessWidget {
--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -1,11 +1,14 @@
+import '../data_sources/local_data_source/local_data_source.dart';
 import '../data_sources/remote_data_source/remote_data_source.dart';
 import '../models/weather.dart';
 
 class WeatherRepository {
   final RemoteDataSource remoteDataSource;
+  final LocalDataSource localDataSource;
 
   WeatherRepository({
     required this.remoteDataSource,
+    required this.localDataSource,
   });
 
   Future<List<Weather>?> getCurrentWeathers() async {
